98c29924b096cf6b49dce69c457e395ba2d63f6d
GLB-MXM0004 <glb-mxm0004@MacBook-Pro-de-GLB-MXM0004.local>
2014-10-18 16:15:09 -0500

    Initial Commit

---
+++ MCMS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCMS
+//
+//  Created by GLB-MXM0004 on 18/10/14.
+//  Copyright (c) 2014 Hugo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
